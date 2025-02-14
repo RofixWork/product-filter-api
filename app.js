ebe2d0c22cc24b3d5097dca1f21874a6ee07131d
Rofix <wourkout123@gmail.com>
2025-02-14 19:03:11 +0100

    add production commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173", "https://product-filter-frontend.vercel.app"]
 }))
 app.use(morgan('dev'))
 
